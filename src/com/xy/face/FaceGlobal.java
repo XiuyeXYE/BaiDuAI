71111e10af33d5e122d25ad5858a1c333bf6e9e5
xiuye <xiuye_engineer@outlook.com>
2019-07-10 15:46:26 +0800

    compare faces

--- src/com/xy/face/FaceGlobal.java
+++ src/com/xy/face/FaceGlobal.java
@@ -3,7 +3,7 @@ package com.xy.face;
 public class FaceGlobal {
 
 	public static final String APP_ID = "16753210";
-	public static final String APP_KEY = "app key";
-	public static final String SECURITY_KEY = "security key";
+	public static final String APP_KEY = "qdzBqbhPGFLkF4kl6leGWXfG";
+	public static final String SECURITY_KEY = "G494UoG2k4GZqgTOQ5iNZGcaelh8fppa";
 	
 }
