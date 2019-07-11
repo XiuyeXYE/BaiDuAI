46dcf6f0609670d114a0ae997560e23101213cfc
xiuye <xiuye_engineer@outlook.com>
2019-07-11 10:54:48 +0800

    none

--- src/com/xy/words/WordsGlobal.java
+++ src/com/xy/words/WordsGlobal.java
@@ -3,8 +3,8 @@ package com.xy.words;
 public class WordsGlobal {
 
 	public static final String APP_ID = "16757430";
-	public static final String APP_KEY = "U9igjj7urLGGmgT1Cm6bu03i";
-	public static final String SECURITY_KEY = "YHYPzPvptGOyNr1CekWWGbWd6O9p2lb3";
+	public static final String APP_KEY = "";
+	public static final String SECURITY_KEY = "";
 	
 	
 }
