eb6ebb4d777a8e6275d2ebf2708fedc0afb14630
xiuye <xiuye_engineer@outlook.com>
2019-07-15 11:24:08 +0800

    业务能力
    H5视频活体检测产品，是由两个接口组合而成，主要用于在H5场景下，通过用户新录制并上传一个视频，来进行活体检测的判断。相对于APP有动作校验+静默图片活体、静默图片活体这两种方式，H5视频活体方案比APP方案更加灵活，同时比单张图片活体方式更加安全。其主要功能如下所示：
    
    视频多帧活体检测：录制并上传的视频，会在云端进行随机抽帧分析，并得出最终的活体检测分数。
    语音校验码：为防止用户提交非当前操作的视频，在录制视频时，随机分配一个数字，用户需要读出这个数字，在后续识别时校验，以判断视频是否为现场录制。语音校验码作为辅助性质的验证条件，是一个可选项，如果应用场景比较嘈杂或方言口音比较重，可以不使用语音验证。
    以上两项能力，分为两个接口，使用顺序为语音校验码->视频多帧校验，具体调用逻辑可以参考我们的。Demo体验地址（电脑端用谷歌、火狐浏览器）：
    Mobile：https://ai.baidu.com/face-verify/mobile.html
    PC：https://ai.baidu.com/face-verify/pc.html
    
    主要适用场景
    
    微信服务号：用于对操作用户真实性要求严格的场景，用于依托于微信服务号的金融开户、实名认证、账户信息变更二次验证等服务。
    APP内Webview：对于如Cordova架构开发的APP，或者APP内变更频繁的身份信息页等情况，可以采用此方案完成活体检测。
    浏览器：如果仅是一个H5宣传页，或者Wap版本网页等，可以通过此方法快速集成更加安全的活体检测功能。
    此方案的优劣势
    
    优势：相对于APP有动作校验、单张图片静默判断，此方法在没有APP情况下，可以更快速、轻量级地实现活体检测，同时保障一定安全性。
    劣势：由于视频较大，上传时间可能较长，另由于不同手机的浏览器内核差异较大，容易出现兼容性问题。

---
+++ src/com/xy/face/demo/Verify.java
@@ -0,0 +1,63 @@
+package com.xy.face.demo;
+
+import com.xy.util.HttpUtil;
+import com.xiuye.util.cls.TypeUtil;
+import com.xy.face.AuthService;
+import com.xy.util.Base64Util;
+import com.xy.util.FileUtil;
+import com.xy.util.GsonUtils;
+
+import java.net.URLEncoder;
+import java.util.*;
+import java.util.Base64.Encoder;
+
+/**
+ * 视频活体检测接口
+ */
+public class Verify {
+
+	/**
+	 * 重要提示代码中所需工具类 FileUtil,Base64Util,HttpUtil,GsonUtils请从
+	 * https://ai.baidu.com/file/658A35ABAB2D404FBF903F64D47C1F72
+	 * https://ai.baidu.com/file/C8D81F3301E24D2892968F09AE1AD6E2
+	 * https://ai.baidu.com/file/544D677F5D4E4F17B4122FBD60DB82B3
+	 * https://ai.baidu.com/file/470B3ACCA3FE43788B5A963BF0B625F3 下载
+	 */
+	public static String faceVerify() {
+		// 请求url
+		String url = "https://aip.baidubce.com/rest/2.0/face/v1/faceliveness/verify";
+		try {
+
+			String param = URLEncoder.encode("video_base64", "UTF-8") + "="
+					+ URLEncoder.encode(Base64Util.encode(FileUtil.readFileByBytes(
+							"C:\\Users\\admin\\Documents\\WeChat Files\\XiuyeXY\\FileStorage\\Video\\2019-07\\eb2d6b5fce3bd266cb9c16bf56ae3f48.mp4")),
+							"UTF-8");
+
+			String accessToken = AuthService.getAuth();
+
+			String result = HttpUtil.post(url, accessToken, param);
+//			System.out.println(GsonUtils.format(result));
+			
+			FileUtil.writeFile("my/result2.json", GsonUtils.format(result).getBytes());
+			
+			@SuppressWarnings("unchecked")
+			Map<String, Object> m = GsonUtils.fromJson(result, Map.class);
+			Map<String, Object> res = TypeUtil.dynamic_cast(m.get("result"));
+			List<Map<String, Object>> picList = TypeUtil.dynamic_cast(res.get("pic_list"));
+
+			for (Map<String, Object> pic : picList) {
+				FileUtil.writeFile("my/"+pic.get("face_id") + ".jpg",
+						com.baidu.aip.util.Base64Util.decode(TypeUtil.dynamic_cast(pic.get("pic"))));
+			}
+
+			return result;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public static void main(String[] args) {
+		Verify.faceVerify();
+	}
+}
