cea5aebbc39719ef182674f18657b3b2c893512e
xiuye <xiuye_engineer@outlook.com>
2019-07-21 22:24:48 +0800

    key

--- src/com/sdk/face/FaceGlobal.java
+++ src/com/sdk/face/FaceGlobal.java
@@ -3,7 +3,7 @@ package com.sdk.face;
 public class FaceGlobal {
 
 	public static final String APP_ID = "16757430";
-	public static final String API_KEY = "c8yZQHblDzqPLUsFSU3IMHsw";
-	public static final String SECURITY_KEY = "r1DAv7QGjiHcU5Xmq4DGDN3SXbG9qH4E";
-	
+	public static final String API_KEY = "";
+	public static final String SECURITY_KEY = "";
+
 }
