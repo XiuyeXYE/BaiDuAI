0c99bed60822b4dea134daa1bebb90864fcccacd
xiuye <xiuye_engineer@outlook.com>
2019-07-22 16:47:04 +0800

    changes

---
+++ src/com/xy/Global.java
@@ -0,0 +1,7 @@
+package com.xy;
+
+public class Global {
+	public static final String APP_ID = "16757430";
+	public static final String APP_KEY = "";
+	public static final String SECURITY_KEY = "";
+}
