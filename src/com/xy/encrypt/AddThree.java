a7d98bbd0e779927bb58d6a1e16267470dd0fa27
xiuye <xiuye_engineer@outlook.com>
2019-07-23 10:29:42 +0800

    modify

--- src/com/xy/encrypt/AddThree.java
+++ src/com/xy/encrypt/AddThree.java
@@ -14,7 +14,7 @@ public class AddThree {
 //		}
 		int total = 26;
 		
-		String s = "hentai";
+		String s = "henddddtai";
 		char []chs = s.toCharArray();
 		LogUtil.log(chs[0]);
 		for(int i=0;i<chs.length;i++) {
